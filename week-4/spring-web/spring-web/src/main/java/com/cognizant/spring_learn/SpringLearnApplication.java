856d3be88d82ee3b17c9793ee1a92aa204c7bb20
Varunreddy5959 <142644775+Varunreddy5959@users.noreply.github.com>
2025-07-15 21:32:32 +0530

    Add files via upload

---
+++ week-4/spring-web/spring-web/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application is starting..."); // <--- this line
+		SpringApplication.run(SpringLearnApplication.class, args);
+	}
+
+}
